13c5fc0b6433ae4f2d12a7d8ec26d83eb9ecdd75
Altryd <michael_bor@mail.ru>
2021-10-09 21:52:29 +0400

    Rewrote the code, don't know what to do with operator[key] right now

--- algorANDSD/hw30_09.cpp
+++ algorANDSD/hw30_09.cpp
@@ -8,6 +8,20 @@
 
 int main()
 {
+	List<Pair<char*, int>> list;
+	Pair<char*, int> add("ky", 5);
+	Pair<char*, int> add2("it's fake!!", 3228);
+	list += add;
+	list.Print();
+	list = list + list + list;
+	list.Print();
+
+	list.PopFront();
+	list.Print();
+	list += add2;
+	Pair<char*, int> ky = list["it's fake!!"];
+	std::cout << ky << std::endl;
+
 	//Pair<char*, int> para("Ky", 5);
 	//std::cout << para;
 	//Pair<char*, int>copy(para);
@@ -42,32 +56,35 @@ int main()
 	//a = a + a + a;
 	//a.Print();
 
-	List<Pair<double, char*>> b;
-	b.PushBack({ 33.4, "yy" });
-	b.PushFront({ 33.3, "kkyk" });
-	b.Print();
-	b = b + b + b + b + b;
-	b.Print();
-	b[(size_t)0] = { 22.8, "zamena" };
-	b.Print();
+	//List<Pair<double, char*>> b;
+	//b.PushBack({ 33.4, "yy" });
+	//b.PushFront({ 33.3, "kkyk" });
+	//b.Print();
+	//b = b + b + b + b + b;
+	//b.Print();
+	//b[(size_t)0] = { 22.8, "zamena" };
+	//b.Print();
 
 
-	List<Pair<char*,char*>> test;
-	test.PushBack({ "ky", "privet" });
-	test.PushFront({ "zdarova", "chelik" });
-	test.Print();
-	test = test + test + test + test;
-	test.Print();
-	test[(size_t)0] = { "eto", "zamena" };
-	test.Print();
-	std::list<int> a;
-	
+	//List<Pair<char*,char*>> test;
+	//test.PushBack({ "ky", "privet" });
+	//test.PushFront({ "zdarova", "chelik" });
+	//test.Print();
+	//test = test + test + test + test;
+	//test.Print();
+	//test[(size_t)0] = { "eto", "zamena" };
+	//test.Print();
+	//std::list<int> a;
 
-	//List<double, int> list;
-	//list.PushBack(5.5, 3);
-	//list.PushFront(11.1, 55);
+
+
+
+
+	//List<Pair<double, int>> list;
+	//list.PushBack({ 5.5, 3 });
+	//list.PushFront({ 11.1, 55 });
 	//list.Print();
-	//List<double, int> list_copy(list);
+	//List<Pair<double, int>> list_copy(list);
 	//list.Clear();
 	//list.PopBack();
 	//list.PopFront();
@@ -75,12 +92,12 @@ int main()
 	//list_copy.PopBack();
 	//list_copy.PopFront();
 	//list_copy.Print();
-	//list.PushBack(5.5, 3);
-	//list.PushFront(11.1, 55);
-	//list.PushBack(3.3, 88);
-	//list.PushFront(99.9, 99);
+	//list.PushBack({ 5.5, 3 });
+	//list.PushFront({11.1, 55});
+	//list.PushBack({ 3.3, 88 });
+	//list.PushFront({ 99.9, 99 });
 	//list.Print();
-	//List<double, int> list_assigment;
+	//List<Pair<double, int>> list_assigment;
 	//list_assigment = list;
 	//list.Clear();
 	//list.Print();
@@ -92,7 +109,7 @@ int main()
 	//list.Print();
 	//list_assigment += list;
 	//list_assigment.Print();
-	//List<double, int> sum;
+	//List<Pair<double, int>> sum;
 	//sum = list_assigment + list_assigment + list;
 	//sum.Print();
 }
