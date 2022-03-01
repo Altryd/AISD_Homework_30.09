056949a0a95b93f13858ab92a5232fdca498e8f6
Altryd <michael_bor@mail.ru>
2022-03-01 16:55:57 +0400

    Create balanced tree, find_iterative

--- algorANDSD/Source.cpp
+++ algorANDSD/Source.cpp
@@ -1,232 +1,65 @@
 #include <iostream>
-#include <list>
 #include <vector>
-#include <string>
-
-std::pair<std::list<int>::const_iterator, std::list<int>::const_iterator> LongestNonDecreasingSequence(const std::list<int>& list)
-{
-	std::list<int>::const_iterator start_of_result = list.begin();
-	std::list<int>::const_iterator end_of_result = list.begin();
-	size_t length = 0;
-	for (auto it = list.begin(); it != list.end(); ++it)
-	{
-		auto unmovable_it = it;
-		auto moveable_it = unmovable_it;
-		moveable_it++;
-		size_t temp_length = 1;
-		while (moveable_it != list.end() && *moveable_it >= *unmovable_it)
-		{
-			unmovable_it = moveable_it;
-			moveable_it++;
-			temp_length++;
-		}
-		if (temp_length > length)
-		{
-			length = temp_length;
-			start_of_result = it;
-			end_of_result = unmovable_it;
-		}
-	}
-	return std::pair<std::list<int>::const_iterator, std::list<int>::const_iterator>(start_of_result, end_of_result);
-}
-
-std::string EraseRecurringWords(std::string text)
-{
-	std::string alphabet = "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-	std::string prev, cur;
-	size_t index_prev_str = 0;
-	for (size_t i = 0; i < text.size(); i++)
-	{
-		if (alphabet.find(tolower(text[i])) != alphabet.npos)
-		{
-			cur.push_back(text[i]);
-			if (cur.size() == 1) index_prev_str = i;
-		}
-		else
-		{
-			if (prev == cur)
-			{
-				text.erase(index_prev_str, prev.size()+1);
-				i -= prev.size()+1;
-			}
-			if (!cur.empty())
-			{
-				prev = cur;
-				cur.clear();
-			}
-		}
-	}
-	if (prev == cur) text.erase(index_prev_str, prev.size());
-	return text;
-}
-enum param
-{
-	AUTHOR,
-	YEAR,
-	PAGES,
-	TITLE
-};
-class Book
+class Node
 {
 private:
-	std::string author;
-	size_t year;
-	size_t pages_number;
-	std::string title;
+	int val;
+	Node* left;
+	Node* right;
 public:
-	friend static std::vector<Book> find_books(const std::vector<Book>& books, int needed_param, std::string compare);
-	static std::vector<Book> random_notes();
-	friend std::ostream& operator<<(std::ostream& out, const Book& book);
+	bool find_iterative(Node* node, int key)
+	{
+		while (node != nullptr)
+		{
+			if (key < node->val)
+			{
+				node = node->left;
+			}
+			else if (key > node->val)
+			{
+				node = node->right;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+	static Node* create_balanced_tree(int* arr, int size)
+	{
+		if (size <= 0) return nullptr;
+		Node* root = new Node();
+		root->val = arr[size / 2];
+		root->left = create_balanced_tree(arr, size / 2);
+		root->right = create_balanced_tree(arr + (size / 2)+1, size - (size / 2) - 1);
+		return root;
+	}
+
+	static void PrintTree(Node* root, int offset = 2)
+	{
+		std::string offset_string(offset, ' ');
+		if (!root)
+		{
+			std::cout << offset_string << "NULL" << std::endl;
+			return;
+		}
+		std::cout << offset_string << root->val << std::endl;
+		std::cout << offset_string << "LEFT: ";
+		PrintTree(root->left, offset + 2);
+		std::cout << offset_string << "RIGHT: ";
+		PrintTree(root->right, offset + 2);
+	}
+
 };
 
 
-std::ostream& operator<<(std::ostream& out, const Book& book)
-{
-	out << "Author " << book.author << ' ';
-	out << "Year " << book.year << ' ';
-	out << "Pages " << book.pages_number << ' ';
-	out << "Title " << book.title << ' ';
-	out << std::endl;
-	return out;
-}
-std::vector<Book> find_books(const std::vector<Book>& books, int needed_param, std::string compare)
-{
-	std::vector<Book> res;
-	switch (needed_param)
-	{
-	case param::AUTHOR:
-	{
-		for (auto book : books)
-		{
-			if (book.author == compare)
-			{
-				res.push_back(book);
-			}
-		}
-		return res;
-		break;
-	}
-	case param::YEAR:
-	{
-		for (auto book : books)
-		{
-			if (std::to_string(book.year) == compare)
-			{
-				res.push_back(book);
-			}
-		}
-		return res;
-		break;
-	}
-	case param::PAGES:
-	{
-		for (auto book : books)
-		{
-			if (std::to_string(book.pages_number) == compare)
-			{
-				res.push_back(book);
-			}
-		}
-		return res;
-		break;
-	}
-	case param::TITLE:
-	{
-		for (auto book : books)
-		{
-			if (book.title == compare)
-			{
-				res.push_back(book);
-			}
-		}
-		return res;
-		break;
-	}
-	default:
-	{
-		return res;
-		break;
-	}
-	}
-}
 
-std::vector<Book> Book::random_notes()
-{
-	std::vector<Book> res;
-	for (size_t i = 0; i < 1000; i++)
-	{
-		std::vector<std::string> authors{ "Антон Чехов", "Николай Ахшарумов", "Юля Волкодав", "Сергей Удалин", "Алексей Будищев" };
-		std::vector<std::string> titles{ "Тайна секретной двери", "Афганец", "Тайна спирального моста", "Бесы в Париже", "Творец Бога" };
-		Book new_book;
-		new_book.year = rand() % 2022;
-		new_book.pages_number = rand() % 1000 + 10;
-		new_book.author = authors[rand() % 5];
-		new_book.title = titles[rand() % 5];
-		res.push_back(new_book);
-	}
-	return res;
-}
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	std::list<int> list;
-	for (size_t i = 0; i < 5; i++)
-	{
-		list.push_front(rand() % 20);
-	}
-	for (auto it : list)
-	{
-		std::cout << it << " , ";
-	}
-	std::cout << std::endl;
-	std::pair<std::list<int>::const_iterator, std::list<int>::const_iterator> lis = LongestNonDecreasingSequence(list);
-	for (auto it = lis.first; it != std::next(lis.second); ++it)
-	{
-		std::cout << *it << std::endl;
-	}
-	
-	std::cout << std::endl << "FIFTH TASK" << std::endl;
-	std::string text = "Привет друг как у тебя дела? Есть ли изменения, изменения, изменения, если да, то напиши!";
-	for (auto char_ : text)
-	{
-		std::cout << char_;
-	}
-	std::cout << std::endl;
-	text = EraseRecurringWords(text);
-	for (auto char_ : text)
-	{
-		std::cout << char_;
-	}
-	std::cout << std::endl;
-	std::cout << std::endl << "SIXTH TASK" << std::endl;
-	std::vector<Book> books = Book::random_notes();
-	for (auto book : books)
-	{
-		std::cout << book;
-	}
-	std::cout << std::endl << std::endl;
-	std::cout << std::endl << "Ищем Николай Ахшарумов" << std::endl;
-	std::vector<Book> needed_books = find_books(books, AUTHOR, "Николай Ахшарумов");
-	for (auto book : needed_books)
-	{
-		std::cout << book;
-	}
-	std::cout << std::endl << "Ищем год 1428" << std::endl;
-	std::vector<Book> needed_books_1995 = find_books(books, YEAR, "1428");
-	for (auto book : needed_books_1995)
-	{
-		std::cout << book;
-	}
-	std::cout << std::endl << "Ищем c 912 страницами" << std::endl;
-	std::vector<Book> needed_books_912 = find_books(books, PAGES, "912");
-	for (auto book : needed_books_912)
-	{
-		std::cout << book;
-	}
-	std::cout << std::endl << "Ищем c названием Бесы в Париже" << std::endl;
-	std::vector<Book> needed_books_title = find_books(books, TITLE, "Бесы в Париже");
-	for (auto book : needed_books_title)
-	{
-		std::cout << book;
-	}
+	int arr[] = { 1, 3 ,5,8, 228 , 1337, 2022, 133228, 555454543 };
+	Node* tree = Node::create_balanced_tree(arr, 9);
+	tree->PrintTree(tree);
+	return 0;
 }
